930b207f65e0290c12a7e8692b754f315ed78261
Peng.Li <nowind_lee@qq.com>
2016-11-30 17:53:18 +0800

    add servlets for the pages

---
+++ src/main/java/demo/views/LoginPageServlet.java
@@ -0,0 +1,16 @@
+package demo.views;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginPageServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+    }
+
+}
